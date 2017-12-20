1c17e93397489267e79fba87e29a11a11009bc82
Steve Cotonnec <steve.cotonnec@imagina.io>
2017-12-20 16:52:00 +0100

    Update CroppableImageType.php

--- Form/Type/CroppableImageType.php
+++ Form/Type/CroppableImageType.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Comur\ImageBundle\Form\Type;
+namespace ImaginaLabs\IMGImageBundle\Form\Type;
 
 use Symfony\Component\Form\AbstractType;
 // use Symfony\Component\Form\FormBuilder;
