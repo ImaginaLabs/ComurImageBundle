2905a64e926718a63fe0e52b9db60f3a677a4207
Steve Cotonnec <steve.cotonnec@imagina.io>
2017-12-20 16:50:37 +0100

    Update UploadController.php

--- Controller/UploadController.php
+++ Controller/UploadController.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Comur\ImageBundle\Controller;
+namespace ImaginaLabs/IMGImageBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
@@ -11,7 +11,7 @@ use Symfony\Component\HttpFoundation\StreamedResponse;
 
 use Symfony\Component\Finder\Finder;
 
-use Comur\ImageBundle\Handler\UploadHandler;
+use ImaginaLabs/IMGImageBundle\Handler\UploadHandler;
 
 class UploadController extends Controller
 {
